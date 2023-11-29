441da6e77b7ef3ed927b9d9db727fa141d4ad928
raunakkumarsingh <raunakkumar.india@gmail.com>
2023-11-29 13:07:36 +0530

    create ui

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,27 @@
+"use client"
+import React from 'react'
+import { Inbox } from 'lucide-react';
+import { useDropzone } from 'react-dropzone'
+
+function FileUpload() {
+    const {getRootProps, getInputProps}=useDropzone({
+        accept:{"application/pdf": [".pdf"] },
+        maxFiles:1,
+        onDrop(acceptedFiles) {
+            console.log(acceptedFiles);
+        },
+    });
+  return (
+   <div className="p-2 bg-white rounded-xl">
+    <div {...getRootProps({ className: 'border-dashed border-2 rounded-xl curser-pointer bg-gray-50 py-8 flex justify-center items-center flex-col'})}>
+        <input {...getInputProps()}/>
+        <>
+        <Inbox className="w-10 h-10 text-blue-500" />
+         <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+        </>
+    </div>
+  </div>
+  )
+}
+
+export default FileUpload
